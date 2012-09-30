1f5afab541fc9845be384e18da11922adec533a6
Chris Marshall <chris@chrismar035.com>
2012-09-30 16:03:16 -0400

    projection scope, recompile assets

--- app/models/projection.rb
+++ app/models/projection.rb
@@ -2,6 +2,7 @@ class Projection < ActiveRecord::Base
   belongs_to :player
 
   scope :for_week, lambda {|week| where(week: week) }
+  default_scope order('standard desc')
 
   attr_accessible :ppr, :ppr_high, :ppr_low, :rank, :standard, :standard_high,
     :standard_low, :week, :player_id
