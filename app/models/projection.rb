012a3cdce74c7104807d04e25e62b89d4aadf5b1
Chris Marshall <chris@chrismar035.com>
2012-10-13 19:08:49 -0400

    Change default proj scope to by_points

--- app/models/projection.rb
+++ app/models/projection.rb
@@ -2,7 +2,7 @@ class Projection < ActiveRecord::Base
   belongs_to :player
 
   scope :for_week, lambda {|week| where(week: week) }
-  default_scope order('standard desc')
+  scope :by_points, order('standard desc')
 
   attr_accessible :ppr, :ppr_high, :ppr_low, :rank, :standard, :standard_high,
     :standard_low, :week, :player_id
